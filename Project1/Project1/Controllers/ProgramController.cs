7b3f7c393dadf6fa1dbe109b7d754eadcb8bb108
Daniel Keele <dkeele5@gmail.com>
2017-11-08 17:42:51 -0700

    Added some file structure for all my favorite friends

---
+++ Project1/Project1/Controllers/ProgramController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1.Controllers
+{
+    public class ProgramController : Controller
+    {
+        // GET: Program
+        public ActionResult Program()
+        {
+
+            return View();
+        }
+
+        public ActionResult ProgramView(string ProName, string InternName, string AgeGroup, string WeekdayTime, string ProDesc)
+        {
+            Viewbag.ProName = ProName;
+            Viewbag.InternName = InternName;
+            Viewbag.AgeGroup = AgeGroup;
+            Viewbag.WeekdayTime = WeekdayTime;
+            Viewbag.ProDesc = ProDesc;
+
+            return View();
+        }
+    }
+}
