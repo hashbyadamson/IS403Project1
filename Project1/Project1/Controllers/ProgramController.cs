fc86c8da18c1a9f33f195d9bbb213bad86179c17
Conner Olsen <cobackup123@gmail.com>
2017-11-13 16:47:15 -0700

    Conner changes

--- Project1/Project1/Controllers/ProgramController.cs
+++ Project1/Project1/Controllers/ProgramController.cs
@@ -8,20 +8,42 @@ namespace Project1.Controllers
 {
     public class ProgramController : Controller
     {
+        private object viewBag;
+
         // GET: Program
+
+        //Creates and assigns values to the programList List
+        public IEnumerable<SelectListItem> ProgramDropDown()
+        {
+            List<SelectListItem> programList = new List<SelectListItem>();
+            //adds the programs to the programList
+            //In future it can grab from the model instead of hard coding
+            programList.Add(new SelectListItem{Text = "Music",Value = "1"});
+            programList.Add(new SelectListItem{Text = "Personal Finance",Value = "2"});
+            programList.Add(new SelectListItem{Text = "Zumba",Value = "3"});
+
+            //puts the list into an IEnumerable returnProgramList and returns it
+            IEnumerable<SelectListItem> returnProgramList = programList;
+
+            return returnProgramList;
+        }
+
+
         public ActionResult Program()
         {
-
+            //ViewModel viewModel = new ViewModel();
+            ViewBag.Program = ProgramDropDown();
+            
             return View();
         }
 
         public ActionResult ProgramView(string ProName, string InternName, string AgeGroup, string WeekdayTime, string ProDesc)
-        {
+        {/*
             Viewbag.ProName = ProName;
             Viewbag.InternName = InternName;
             Viewbag.AgeGroup = AgeGroup;
             Viewbag.WeekdayTime = WeekdayTime;
-            Viewbag.ProDesc = ProDesc;
+            Viewbag.ProDesc = ProDesc;*/
 
             return View();
         }
---
+++ Project1/Project1/Controllers/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Project1.Controllers
+{
+    internal class ViewModel
+    {
+        public ViewModel()
+        {
+        }
+
+        public static IEnumerable<SelectListItem> Program { get; internal set; }
+    }
+}
