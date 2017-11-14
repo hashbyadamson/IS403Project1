d2c8932f740f508185b640bd0edb189a1ff333cf
Your Name <hashbyadamson@gmail.com>
2017-11-13 17:22:00 -0700

    nothing

--- Project1/Project1/Controllers/ProgramController.cs
+++ Project1/Project1/Controllers/ProgramController.cs
@@ -8,7 +8,6 @@ namespace Project1.Controllers
 {
     public class ProgramController : Controller
     {
-        // GET: Program
 
         //Creates and assigns values to the programList List
         public IEnumerable<SelectListItem> ProgramDropDown()
@@ -16,7 +15,7 @@ namespace Project1.Controllers
             List<SelectListItem> programList = new List<SelectListItem>();
             //adds the programs to the programList
             //In future it can grab from the model instead of hard coding
-            programList.Add(new SelectListItem{Text = "Music",Value = "1"});
+            programList.Add(new SelectListItem{Text = "Music", Value = "1"});
             programList.Add(new SelectListItem{Text = "Personal Finance",Value = "2"});
             programList.Add(new SelectListItem{Text = "Zumba",Value = "3"});
 
@@ -24,19 +23,20 @@ namespace Project1.Controllers
             IEnumerable<SelectListItem> returnProgramList = programList;
 
             return returnProgramList;
-        }
 
+        }
 
         public ActionResult Program()
         {
             //ViewModel viewModel = new ViewModel();
             ViewBag.Program = ProgramDropDown();
-            
+
             return View();
         }
 
         public ActionResult ProgramView(string ProName, string InternName, string AgeGroup, string WeekdayTime, string ProDesc)
         {
+
             ViewBag.ProName = ProName;
             ViewBag.InternName = InternName;
             ViewBag.AgeGroup = AgeGroup;
